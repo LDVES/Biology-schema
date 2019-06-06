2ae2751341b5971e56ff990aac57d75f8fc21ac4
Kubiz <kubaploch34@gmail.com>
2019-06-06 23:22:23 +0200

    Create an unity project

---
+++ BiologyProject2019/Assets/ClickDetector.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ClickDetector : MonoBehaviour
+{
+    public GameObject Popup;
+    public TextMeshPro TestText;
+
+    private void OnMouseDown()
+    {
+        print(TestText.text =  gameObject.name);
+    }
+}
